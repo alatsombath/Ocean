20a09e1d39ec538fa03db17d6a4d07823cb759ca
alatsombath <alatsombath@users.noreply.github.com>
2015-04-01 16:41:17 -0700

    1.2 (25 March 2015 rev 2)
    - Smoother waveforms through cubic interpolation!
    - Fixed unaccounted pixels for antialiasing trick
    - Real fix for repeated histogram tail values by ignoring last measure
    value and double padding ends of spectrum

--- Skins/Ocean/Ocean/@Resources/Ocean.lua
+++ Skins/Ocean/Ocean/@Resources/Ocean.lua
@@ -1,4 +1,4 @@
--- Ocean v1.1
+-- Ocean v1.2
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
 local Measure,MeasureBuffer,cos,PI={},{},math.cos,math.pi
@@ -12,21 +12,37 @@ function Initialize()
 	for i=Index,Limit do Measure[i]=SKIN:GetMeasure((gsub(MeasureName,Sub,i))) end end
 
 -- http://paulbourke.net/miscellaneous/interpolation/
-local function CosineInterpolate(y1,y2,mu)
-	local mu2=(1-cos(mu*PI))/2 return (y1*(1-mu2)+y2*mu2) end
+local function CubicInterpolate(y0,y1,y2,y3,mu)
+   local mu2,a0=mu*mu,y3-y2-y0+y1
+   local a1=y0-y1-a0
+   return (a0*mu*mu2+a1*mu2+(y2-y0)*mu+y1) end
 
 function Update()
 	for i=Index,Limit do MeasureBuffer[i]=Measure[i]:GetValue() end 
 	
-	for i=Index,Limit-1 do
+	local y0,y1,y2,y3,LocalMu=MeasureBuffer[1],MeasureBuffer[1],MeasureBuffer[2],MeasureBuffer[4],0
+	for j=1,InterWidth*2 do
+		LocalMu=LocalMu+Mu*0.5
+		SKIN:Bang("!SetOption","MeasureIter","Formula",CubicInterpolate(y0,y1,y2,y3,LocalMu))
+		SKIN:Bang("[!UpdateMeasure MeasureIter][!UpdateMeter MeterHistogram][!UpdateMeter MeterLine]")
+	end
+	
+	for i=2,Limit-3 do
 		SKIN:Bang("!SetOption","MeasureIter","Formula",MeasureBuffer[i])
 		SKIN:Bang("[!UpdateMeasure MeasureIter][!UpdateMeter MeterHistogram][!UpdateMeter MeterLine]")
 		
-		local y1,y2,LocalMu=MeasureBuffer[i],MeasureBuffer[i+1],0
-		for j=0,InterWidth do
+		local y0,y1,y2,y3,LocalMu=MeasureBuffer[i-1],MeasureBuffer[i],MeasureBuffer[i+1],MeasureBuffer[i+2],0
+		for j=1,InterWidth do
 			LocalMu=LocalMu+Mu
-			SKIN:Bang("!SetOption","MeasureIter","Formula",CosineInterpolate(y1,y2,LocalMu))
+			SKIN:Bang("!SetOption","MeasureIter","Formula",CubicInterpolate(y0,y1,y2,y3,LocalMu))
 			SKIN:Bang("[!UpdateMeasure MeasureIter][!UpdateMeter MeterHistogram][!UpdateMeter MeterLine]")
 		end
+	end
+	
+	y0,y1,y2,y3,LocalMu=MeasureBuffer[Limit-4],MeasureBuffer[Limit-2],MeasureBuffer[Limit-1],MeasureBuffer[Limit-1],0
+	for j=1,InterWidth*2 do
+		LocalMu=LocalMu+Mu*0.5
+		SKIN:Bang("!SetOption","MeasureIter","Formula",CubicInterpolate(y0,y1,y2,y3,LocalMu))
+		SKIN:Bang("[!UpdateMeasure MeasureIter][!UpdateMeter MeterHistogram][!UpdateMeter MeterLine]")
 	end
 end
